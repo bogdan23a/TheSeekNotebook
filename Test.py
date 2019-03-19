3739b3a67fae69fa5c75468c2077d57a8a195638
bogdan23a <gbogdan23a@yahoo.com>
2019-03-19 02:08:30 +0200

    Testing Update
    Added tests for Auth and Request methods

--- Test.py
+++ Test.py
@@ -1,3 +1,45 @@
-def testSum():
-    assert sum([1,2,3]) == 6
-    
+import unittest
+import SEEK
+
+PROT_DEFAULT_AUTHENTICATION_STRING = "DEFAULT"
+
+class TestSEEK(unittest.TestCase):
+
+    def setUp(self):
+
+        self.auth = (PROT_DEFAULT_AUTHENTICATION_STRING,
+                     PROT_DEFAULT_AUTHENTICATION_STRING)
+
+        self.testObject = SEEK.module(self.auth)
+
+    def tearDown(self):
+
+        del self.auth
+
+        del self.testObject
+        
+    # Testing the authentication
+    def test_AuthWorking(self):
+        
+        self.assertEqual(self.testObject.session.auth,
+                        (PROT_DEFAULT_AUTHENTICATION_STRING,
+                        PROT_DEFAULT_AUTHENTICATION_STRING), 
+                        "Request should have these credentials")
+
+    def test_AuthNotWorking(self):
+        
+    # Testing the request of an existent object
+    def test_RequestReturnsTrue(self):
+
+        self.assertTrue(self.testObject.request(type="assays", id="576"), 
+                        "This request should return true")
+
+    # Testing the request of an inexistent object
+    def test_RequestReturnsFalse(self):
+
+        self.assertFalse(self.testObject.request(type="assays", id="0"), 
+                        "This request should return false")
+
+if __name__ == '__main__':
+
+    unittest.main()
