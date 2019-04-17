7bda52073d8ef9f13be81666537fde345e407ca8
bogdan23a <gbogdan23a@yahoo.com>
2019-04-17 15:13:41 +0300

    Adding documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
