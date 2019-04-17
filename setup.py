f03a530e277d1726a05c6f4e4820723b885ef9aa
bogdan23a <gbogdan23a@yahoo.com>
2019-04-18 00:47:49 +0300

    Dependency setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+
+    name="SEEK",
+    version="1.0",
+    install_requires=[
+        "sphinx",
+        "ipykernel",
+        "nbsphinx",
+        "ipywidgets",
+        "pandas",
+        "requests",
+        "tabulate",
+    ],
+)
